4195e857d8627f4a4d35098edd91862a973873ca
Omer Citik <omer.citik@hva.nl>
2022-02-27 14:20:16 +0100

    feat: Fonts defined and injected start of application

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
+import Header from './components/Header';
+import MainScreen from './screens/MainScreen';
+
+import TitleText from './components/TitleText';
+import BodyText from './components/BodyText';
+
+// Define All Custom Fonts AND require them
+const fetchFonts = () => {
+  return Font.loadAsync({
+    // "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    // "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    // "chocolatte": require("./assets/fonts/Chocolatte.otf"),
+    // "questrial": require("./assets/fonts/Questrial-Regular.ttf"),
+    "balsamiq-regular": require("./assets/fonts/BalsamiqSans-Regular.ttf"), // Primary, good practice set this in js object
+    "balsamiq-bold": require("./assets/fonts/BalsamiqSans-Bold.ttf"),       // Secondary
+  })
+}
 
 export default function App() {
+
+  // Before Loading the app inject fonts used first
+  const [dataLoaded, setDataLoaded] = useState(false)
+  if (!dataLoaded) { return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} onError={(err) => console.log(err)} />; }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Introduction" />
+      <MainScreen />
     </View>
   );
 }
@@ -13,8 +39,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
 });
